181b2f23ada90d70f5df513ed303ef9eb30adbb0
Mark Evans <mark.laravel.coder@gmail.com>
2023-04-12 18:13:21 +0800

    first commit

---
+++ tests/Feature/Question2Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class Question2Test extends TestCase
+{
+    use RefreshDatabase;
+    
+    public function test_landing_page_renders(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
